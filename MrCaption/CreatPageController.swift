7ea981158141468a4ab28ee1e9f097788051d03e
Aka.Fido <wanghailin317@gmail.com>
2014-12-09 11:15:38 +0800

    Add View Controller for Create Page

---
+++ MrCaption/CreatPageController.swift
@@ -0,0 +1,25 @@
+//
+//  CreatPageController.swift
+//  MrCaption
+//
+//  Created by Fido Wang on 9/12/14.
+//  Copyright (c) 2014 YlLUN WAN. All rights reserved.
+//
+
+import UIKit
+
+class CreatPageController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
